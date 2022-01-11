c7317025d35290935eb15011f9b2c2eeb6960c19
anikrauth <anikrouthwebdev@gmail.com>
2022-01-12 01:31:41 +0600

    ddd

--- src/api.js
+++ src/api.js
@@ -22,13 +22,14 @@ function Apitest() {
         
         <div className="mdc-card__primary-action">
             <div>
-                <h1>Cloud over: {data.current.cloudcover}</h1>
+                {/* <h1>Cloud over: {data.current.cloudcover}</h1>
                 <h1>Feels Like: {data.current.feelslike}</h1>
                 <h1>Humidity: {data.current.humidity}</h1>
                 <h1>Is this Day?: {data.current.is_day}</h1>
                 <h1>Observation time: {data.current.observation_time}</h1>
                 <h1>What is the temperature: {data.current.temperature}°c</h1>
-                <h1><span><img src={data.current.weather_icons[0]} alt="dnsd"/></span> {data.current.weather_descriptions}</h1>
+                <h1><span><img src={data.current.weather_icons[0]} alt="dnsd"/></span> {data.current.weather_descriptions}</h1> */}
+                <h1>Yes its workiing</h1>
             </div>
         </div>
     )
